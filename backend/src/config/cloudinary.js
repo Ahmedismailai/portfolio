c0728fab38275540aa862c27b88aa7e1778fa1cf
Ahmed Ismail <sobujmd044@gmail.com>
2026-07-30 20:55:15 +0600

    Add fallback Cloudinary credentials in backend code for automatic image upload support on Render

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "dlvexpunm",
+  api_key: process.env.CLOUDINARY_API_KEY || "867636738411388",
+  api_secret: process.env.CLOUDINARY_API_SECRET || "Tuko6m9h4ogZsQ3Y_87UfGqv3-E",
 });
 
 module.exports = cloudinary;
